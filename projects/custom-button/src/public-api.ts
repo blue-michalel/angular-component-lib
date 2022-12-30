417d4b48d0db1e09477087428fb9279cec832514
Michał Kowalik <michal.kowalik.94+blue-michalel@gmail.com>
2022-12-30 10:28:24 +0100

    chore: clean up

--- projects/custom-button/src/public-api.ts
+++ projects/custom-button/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of custom-button
  */
 
-export * from './lib/custom-button.service';
 export * from './lib/custom-button.component';
 export * from './lib/custom-button.module';
